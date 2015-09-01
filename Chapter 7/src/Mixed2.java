4a5bb2c9608ee2fa7bd3bf99eed35744e2769327
marcwebdude <marcus.faye@yahoo.com>
2015-09-01 16:14:08 -0500

    Chapter 7 Examples

---
+++ Chapter 7/src/Mixed2.java
@@ -0,0 +1,18 @@
+
+public class Mixed2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A a = new A();
+		B b = new B();
+		C c = new C();
+		A a2 = new C();
+		
+		// candidate code goes here (three lines)
+		a2.m1();
+		a2.m2();
+		a2.m3();		
+		// end
+	}
+
+}
