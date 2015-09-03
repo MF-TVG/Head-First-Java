1440fa18c8383ed148a54fc80223e02afc919670
marcwebdude <marcus.faye@yahoo.com>
2015-09-03 14:14:24 -0500

    Chapter 10 Examples

---
+++ Chapter 10/src/NoStatic.java
@@ -0,0 +1,11 @@
+import static java.lang.System.out;
+import static java.lang.Math.*;
+public class NoStatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		out.println("sqrt " + sqrt(2.0));
+		out.println("tan" + tan(60));
+	}
+
+}
